b71fd1a46de2498fc61ef0707ecac19a445fb843
Mitraws <m.ws@outlook.com>
2020-04-28 15:00:41 +0200

    Added Title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
+      <main>
+        <Title content="Some Simple Title" />
+      </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+// src/components/Title.js
+import React from "react";
+
+
+export default function Title(props) {
+    return <h1>{props.content}</h1>;
+  }
+
+//   function Title() {
+//     return <h1>Hi there!</h1>;
