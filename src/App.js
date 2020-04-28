23dff7771d17bda91b95490200e4075ec5138907
Mitraws <m.ws@outlook.com>
2020-04-28 16:49:23 +0200

    added new component pokemon

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
 
 function App() {
   return (
     <div className="App">
       <main>
         <Title content="Some Simple Title" />
+        <Pokemon
+          name="Charizard"
+          weight="90"
+          awesome="yes"
+          terrifying="yes"
+          abilities={["Blaze", "Solar power", "Tough claws", "Drought"]}
+        />
+        <Pokemon
+          name="Bulbasaur"
+          weight="6.9"
+          awesome="yes"
+          terrifying="yes"
+          abilities="Overgrow, Chlorophyll"
+        />
+        <Pokemon
+          name="Mewtwo"
+          weight="122"
+          awesome="yes"
+          terrifying="no"
+          abilities="Pressure, Unnerve, Steadfast, Insomnia"
+        />
+        <Pokemon
+          name="Mega beedrill"
+          weight="65"
+          awesome="no"
+          terrifying="no"
+          abilities="Intimidate, Unnerve"
+        />
       </main>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
